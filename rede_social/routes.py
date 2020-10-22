a98bcdf59ec835e2da29450564d223499426f4a4
Larabsg18 <lara.beatriz.soares03@aluno.ifce.edu.br>
2020-10-22 21:10:17 +0000

    Atualização de telas de teste

--- rede_social/routes.py
+++ rede_social/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, redirect, url_for, request
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
