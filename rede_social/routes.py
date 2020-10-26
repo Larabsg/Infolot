a0c46d5160104956982bffbfeac4c3239a2890ec
Larabsg18 <lara.beatriz.soares03@aluno.ifce.edu.br>
2020-10-26 01:25:23 +0000

    Mudança de layout nas telas

--- rede_social/routes.py
+++ rede_social/routes.py
@@ -3,8 +3,21 @@ from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario_logado = False
+    return render_template('index.html', logado = usuario_logado)
 
-@app.route('/about')
+@app.route('/sobre')
 def aboutpage():
     return render_template('about.html')
+
+@app.route('/contato')
+def contactpage():
+    return render_template('contact.html')
+
+@app.route('/login')
+def loginpage():
+    return render_template('login.html')
+
+@app.route('/cadastro')
+def registerpage():
+    return render_template('register.html')
