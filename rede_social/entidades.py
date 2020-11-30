300fae5958b552b61c13e6c276de0c6ea12bbcc2
Larabsg <laragomes2009@hotmail.com>
2020-11-30 13:54:28 -0300

    Configuração de banco de dados (incompleta)

---
+++ rede_social/entidades.py
@@ -0,0 +1,6 @@
+from rede_social import db
+
+class Usuario(db.Models):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unic=True, nullabel=False)
+    senha = db.Column(db.String(1000), nullabel=False)
